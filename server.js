475b6b1822a2cb5ac604d1b22574d13c4b29ed70
Vin88 <vinothinikings@gmail.com>
2018-11-14 16:57:20 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+"/views/partials");
+app.set("view engine", "hbs");
+//Setup middleware
+app.use(express.static(__dirname+'/public'));
+
+//Registering Middleware
+app.use((req, res, next) => {
+    let _date = new Date().toString();
+    console.log(_date);
+    fs.appendFile("server.log", _date + "\n", (err) => {
+        if(err){
+            console.log(err);
+        }
+    });
+    next();
+});
+
+app.get("/", (req, res) => {
+    // res.send("Happy World");
+    res.render('home.hbs', {
+        pageTitle: "Home Page !!!",
+        welcomeMsg: "WElcome to the world !!!"
+    });
+});
+app.get("/userInfo", (req, res) => {
+    res.send({
+        name: "Vinothini",
+        work: "Senior Software Engg"
+    });
+});
+app.get("/about", (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: "About Page !!!"
+    });
+});
+
+//HBS Helper functions
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+app.listen(3000, () => {
+    console.log("Server is up on port 3000");
+});
